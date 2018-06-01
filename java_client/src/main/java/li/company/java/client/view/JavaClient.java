a7b484c937990cd306da03c393b3b61140005d59
Philip Ekblom <philip.ekblom@gmail.com>
2018-06-02 00:37:37 +0200

    Update JavaClient.java

--- java_client/src/main/java/li/company/java/client/view/JavaClient.java
+++ java_client/src/main/java/li/company/java/client/view/JavaClient.java
@@ -6,7 +6,8 @@ import li.company.java.client.model.SocketClient;
 
 public class JavaClient implements Runnable, SocketClient {
 
-    private static final String SERVER = "http://213.67.247.197:5000";
+    private static final int DEFAULT_SERVER = "localhost";
+    private static final int DEFAULT_PORT = 5000;
     private boolean running = false;
     private Connection connection = new Connection(this);
     private final Scanner console = new Scanner(System.in);
@@ -21,11 +22,18 @@ public class JavaClient implements Runnable, SocketClient {
 
     @Override
     public void run() {
-        connection.connect(SERVER);
         while (running) {
             String input = console.nextLine();
             String[] commands = input.split(" ");
             switch (commands[0]) {
+                case "connect":
+                    if (commands.length > 2) {
+                        connection.connect(commands[1] + ":" + commands[2]);
+                    } else if (commands.length = 1)
+                        connection.connect(commands[1] + ":" + DEFAULT_PORT);
+                    } else {
+                        connection.connect(DEFAULT_SERVER + ":" + DEFAULT_PORT);
+                    }
                 case "exit":
                     connection.exit();
                     break;
